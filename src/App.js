71bb50da2e22aab8330434663b120d2a70fde583
homeis <homeis@homeiss-MacBook-Pro.local>
2019-12-17 12:35:55 +0200

    render communities

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const AppStyled = styled.div`
+  text-align: center;
+
+  .countries-list {
+    list-style-type: none;
+  }
+`
+
+class App extends React.Component {
+
+  state= {
+    name: [],
+    originUpperLevels: []
+  }
+
+  componentDidMount() {
+    fetch('http://api.homeis.com/v1/communities')
+      .then(res => res.json())
+      .then(res => {
+        console.log(res.data);
+        this.setState({name: res.data, originUpperLevels: res.data.originUpperLevels})
+      });
+  }
+
+  render() {
+    const { name, originUpperLevels } = this.state;
+    console.log(name);
+    return (
+      <AppStyled className="App">
+        <h1>Comunities</h1>
+        <ul>
+          {name.map( obj => <li className="countries-list">{obj.name}</li> )}
+        </ul>
+      </AppStyled>
+    );
+  }  
 }
 
 export default App;
